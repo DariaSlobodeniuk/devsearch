ad907ad017efb16b88589520b30859d64f2dfc28
DariaSlobodeniuk <slobodenuk1@gmail.com>
2021-09-14 11:55:10 +0300

    add signals

--- devsearch/users/models.py
+++ devsearch/users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, blank=True, null=True)
@@ -23,7 +24,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):
@@ -35,3 +36,6 @@ class Skill(models.Model):
 
     def __str__(self):
         return str(self.name)
+
+
+
