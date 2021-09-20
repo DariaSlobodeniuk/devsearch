fa3a1d103f9f702873080f995fb0ca41a2f23d38
DariaSlobodeniuk <slobodenuk1@gmail.com>
2021-09-20 18:36:26 +0300

    sending email

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from django.conf.global_settings import EMAIL_HOST, EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -115,11 +117,18 @@ USE_I18N = True
 
 USE_L10N = True
 
+
 USE_TZ = True
-
-
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'a8devsearch@gmail.com'
+EMAIL_HOST_PASSWORD = 'bblswerjiieblgnm'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
--- devsearch/users/signals.py
+++ devsearch/users/signals.py
@@ -2,7 +2,8 @@ from .models import Profile
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-
+from django.core.mail import send_mail
+from django.conf import settings
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
@@ -10,6 +11,15 @@ def create_profile(sender, instance, created, **kwargs):
     if created:
         user = instance
         profile = Profile.objects.create(user=user, username=user.username, email=user.email, name=user.first_name)
+        subject = "Welcome To DevSearch!"
+        message = "We are glad you are here !!!"
+        send_mail(subject,
+                  message,
+                  settings.
+                  EMAIL_HOST_USER,
+                  [profile.email],
+                  fail_silently=False,)
+
 
 
 @receiver(post_save, sender=Profile)
